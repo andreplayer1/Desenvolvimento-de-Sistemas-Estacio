906310aa1776e80e81adf5d083369466232e63de
André Barbosa <123273445+andreplayer1@users.noreply.github.com>
2026-05-06 22:18:35 -0300

    Atualização do meu CRUD
    Na proxima etapa, estarei colocando tudo isso para ser exibido na pagina com HTML e CSS

--- Desenvolvimento Web/Semana 9/Atividade/app.js
+++ Desenvolvimento Web/Semana 9/Atividade/app.js
@@ -32,9 +32,9 @@ let catalogoDeJogos = [
 ]
 
 let adicionarJogo = (titulo, genero, estado) => {
-    
-    let novoId = catalogoDeJogos.length +1;
-    
+
+    let novoId = catalogoDeJogos.length + 1;
+
     let novoJogo =
     {
         id: novoId,
@@ -42,11 +42,38 @@ let adicionarJogo = (titulo, genero, estado) => {
         genero: genero,
         estado: estado
     };
-    
+
     catalogoDeJogos.push(novoJogo);
 }
 
+let atualizarEstado = (id, novoEstado) => {
+    let encontrouJogo = false;
+
+    for (let i = 0; i < catalogoDeJogos.length; i++) {
+
+        if (catalogoDeJogos[i].id === id) {
+            catalogoDeJogos[i].estado = novoEstado;
+            encontrouJogo = true;
+            break;
+        }
+    }
+
+    if(encontrouJogo === false){
+        console.log("Erro: ID Inválido ou não encontrado.");
+    }
+}
+
+let excluirJogo = (idParaExcluir) => {
+    catalogoDeJogos = catalogoDeJogos.filter(jogo => jogo.id !== idParaExcluir);
+}
+
 adicionarJogo("Dragon Ball: Sparking Zero", "Luta", "Na fila");
+
+adicionarJogo("Sifu", "Luta", "A jogar");
+
+atualizarEstado(3, "Jogando");
+
+excluirJogo(5);
 
 for (let i = 0; i < catalogoDeJogos.length; i++) {
     console.log(`O jogo é ${catalogoDeJogos[i].titulo} é do gênero ${catalogoDeJogos[i].genero} e seu estado atual é: ${catalogoDeJogos[i].estado}`)
---
+++ Desenvolvimento Web/Semana 9/Atividade/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+        if (catalogoDeJogos[i].id === id) {
+            catalogoDeJogos[i].estado = novoEstado;
+            encontrouJogo = true;
+            break;
